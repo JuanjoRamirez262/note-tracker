6ca43c168e1d8fad9ec9142ef6c84e8010915ecb
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-10-21 09:51:49 -0700

    code working

---
+++ routes/notes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const notes = express.Router();
+const db = require('../db/db.json');
+const fs = require('fs');
+var uniqid = require('uniqid'); 
+
+notes.use(express.json());
+notes.use(express.urlencoded({ extended: true }));
+
+notes.get('/', (req, res) => {
+  res.json(db)
+  // res.send(JSON.stringify(db))
+})
+
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uniqid()
+    }
+
+    db.push(newNote)
+    const newDb = JSON.stringify(db)
+
+    fs.writeFile('./db/db.json', newDb, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+          `Review for ${title} has been written to JSON file`
+        ))
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    res.status(201).json(response)
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+})
+
+module.exports = notes;
